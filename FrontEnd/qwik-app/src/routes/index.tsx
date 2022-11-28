b3f1c9332ae4136985ce53c22bd7934aa995b6b2
Sergei Golos <sergei.golos@gmail.com>
2022-11-27 21:33:38 -0500

    getting data from the backend via proxy

--- FrontEnd/qwik-app/src/routes/index.tsx
+++ FrontEnd/qwik-app/src/routes/index.tsx
@@ -1,13 +1,44 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useResource$, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
+    const store = useStore<{ name?: string }>({
+        name: undefined,
+    });
+    const feed = useResource$<{
+        content: string;
+        age: number;
+        count: number;
+    }[]>(async ({ track, cleanup }) => {
+        console.log("Getting Data");
+        track(() => store.name);
+
+        const abortController = new AbortController();
+        cleanup(() => abortController.abort('cleanup'));
+        const res = await fetch(`https://localhost:3000/api/post`, {
+            signal: abortController.signal,
+        });
+
+        return res.json();
+    });
+
+
   return (
-    <div>      
-      <Link class="mindblow" href="/flower/">
-        Blow my mind 🤯
-      </Link>
+      <div>      
+          <input onInput$={(e: Event) => (store.name = (e.target as HTMLInputElement).value)} />
+    <div>
+              {feed.loading && <div>Loading age guess...</div>}
+              {!feed.loading &&  (
+            <div>
+                     Loaded:  {feed.promise.then((f) => (
+                    <div>
+                        {f[0].content}
+                    </div>
+                ))}
+            </div>
+        )}
+    </div>
     </div>
   );
 });
--- FrontEnd/qwik-app/vite.config.ts
+++ FrontEnd/qwik-app/vite.config.ts
@@ -1,14 +1,16 @@
 
 import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { qwikCity } from '@builder.io/qwik-city/vite';
+
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig(() => {
-  return {
-      plugins: [qwikCity(), qwikVite(), tsconfigPaths(), basicSsl()],
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+    return {
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), basicSsl()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
@@ -19,12 +21,14 @@ export default defineConfig(() => {
         https: true,
         strictPort: true,
         proxy: {
-            '^/api/.*': {
+            '/api': {
                 target: 'https://localhost:7263',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+                changeOrigin: true, 
+                secure: false, 
+                rewrite: (path) => path.replace(/^\/api/, '')                
             }
-        }         
+        },
+        
     }
   };
 });
